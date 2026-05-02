62277f5032def6ea7c954fc6b9d0d5c4bbfdfb49
Narendra Limbu <narendra.ubmil@gmail.com>
2026-05-02 15:04:08 +0100

    Uncommented the tested code.

--- Simple Interface/publisher_subscriber/src/robot_data_subscriber.cpp
+++ Simple Interface/publisher_subscriber/src/robot_data_subscriber.cpp
@@ -19,9 +19,9 @@ public:
 
 void DataSubscriber::OnDataReceived(const custom_interface::msg::RobotData& data)
 {
-   //std::tm* tm = std::localtime(&data.last_updated);
-   //auto timeStamp = std::format("Hour: {}, Min: {}, Sec: {}", tm->tm_hour, tm->tm_min, tm->tm_sec);
-   //RCLCPP_INFO(get_logger(), "Received: %s, Time stamp: %s", data.robot_name.c_str(), timeStamp.c_str());
+   std::tm* tm = std::localtime(&data->last_updated);
+   std::string timeStamp = std::format("Hour: {}, Min: {}, Sec: {}", tm->tm_hour, tm->tm_min, tm->tm_sec);
+   RCLCPP_INFO(get_logger(), "Received: %s, Time stamp: %s", data->robot_name.c_str(), timeStamp.c_str());
 }
 
 int main(int argc, char* argv[])
